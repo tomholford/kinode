bc3fb1f68049f19db561feba80757c0b1f098e85
dr-frmr <docterformer@protonmail.com>
2023-07-27 00:31:17 +0200

    refine to only check src folders

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ use std::process::Command;
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=process-manager");
-    println!("cargo:rerun-if-changed=terminal");
+    println!("cargo:rerun-if-changed=process-manager/src");
+    println!("cargo:rerun-if-changed=terminal/src");
     let pwd = std::env::current_dir().unwrap();
     Command::new("cargo")
         .args(&["component", "build", &format!("--manifest-path={}/process-manager/Cargo.toml", pwd.display()), "--target", "wasm32-unknown-unknown"])

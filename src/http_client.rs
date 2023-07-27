000c95f7954c739a1a2a8ea07471e2098ffa78a6
dachus <dachus@dachus>
2023-07-27 17:30:57 +0200

    headers added

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,12 +1,14 @@
 use serde::{Serialize, Deserialize};
 use crate::types::*;
 use serde_json::json;
+use std::collections::HashMap;
+use http::header::{HeaderName, HeaderValue, HeaderMap};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct HttpClientRequest {
   uri: String,
   method: String,
-  // headers: Vec<(String, String)>,
+  headers: HashMap<String, String>,
   body: String,
 }
 
@@ -37,7 +39,11 @@ async fn handle_message(
   let Some(value) = wm.message.payload.json.clone() else {
     panic!("http_client: request must have JSON payload, got: {:?}", wm.message);
   };
-  let req: HttpClientRequest = serde_json::from_value(value).unwrap();
+
+  let req: HttpClientRequest = match serde_json::from_value(value) {
+    Ok(req) => req,
+    Err(e) => panic!("http_client: failed to parse request: {:?}", e),
+  };
 
   print_tx.send(Printout {
     verbosity: 0,
@@ -47,15 +53,25 @@ async fn handle_message(
   let client = reqwest::Client::new();
         
   let request_builder = match req.method.to_uppercase()[..].to_string().as_str() {
-      "GET" => client.get(req.uri),
-      "POST" => client.post(req.uri),
-      "PUT" => client.put(req.uri),
+      "GET" =>    client.get(req.uri),
+      "PUT" =>    client.put(req.uri),
+      "POST" =>   client.post(req.uri),
       "DELETE" => client.delete(req.uri),
       _ => panic!("Unsupported HTTP method: {}", req.method),
   };
+
+  // parse headers
+  let mut header_map = HeaderMap::new();
+  for (name, value) in req.headers {
+      let header_name = HeaderName::from_bytes(name.as_bytes())
+          .expect("Failed to convert header name to HeaderName.");
+      let header_value = HeaderValue::from_str(&value.as_str())
+          .expect("Failed to convert header value to HeaderValue.");
+      header_map.insert(header_name, header_value);
+  }
   
   let request = request_builder
-      // .headers(self.headers.clone())
+      .headers(header_map)
       .body(req.body.clone())
       .build().unwrap();
   
@@ -71,5 +87,8 @@ async fn handle_message(
       // Err(Error::from(response.error_for_status().unwrap_err()))
       panic!("foo")
   }
-  // !message tuna http_client {"method": "GET", "uri": "https://google.com", "body": ""}
+  // !message tuna http_client {"method": "GET", "uri": "https://jsonplaceholder.typicode.com/posts", "headers": {}, "body": ""}
+  // !message tuna http_client {"method": "POST", "uri": "https://jsonplaceholder.typicode.com/posts", "headers": {"Content-Type": "application/json"}, "body": "{\"title\": \"foo\", \"body\": \"bar\"}"}
+  // !message tuna http_client {"method": "PUT", "uri": "https://jsonplaceholder.typicode.com/posts", "body": "{\"title\": \"foo\", \"body\": \"bar\"}"}
+
 }

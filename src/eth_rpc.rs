f99f532828ec190819d822e2c3b14294bc781893
dachus <dachus@dachus>
2023-09-05 12:15:20 -0600

    cancelling event subscriptions working

--- src/eth_rpc.rs
+++ src/eth_rpc.rs
@@ -1,5 +1,6 @@
 use crate::types::*;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use std::collections::HashMap;
 use ethers::prelude::*;
 use serde::{Deserialize, Serialize};
@@ -14,7 +15,6 @@ use thiserror::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
 enum EthRpcAction {
-    SubscribeBlocks,
     SubscribeEvents(EthEventSubscription),
     Unsubscribe(u64),
 }
@@ -43,7 +43,7 @@ impl EthRpcError {
 }
 
 pub async fn eth_rpc(
-    our_name: String,
+    our: String,
     rpc_url: String,
     send_to_loop: MessageSender,
     mut recv_in_client: MessageReceiver,
@@ -54,213 +54,64 @@ pub async fn eth_rpc(
     };
 
     // TODO generate subscription IDs and put into here, create a cancel message        
-    let subscription_threads = Arc::new(Mutex::new(HashMap::<u64, tokio::task::JoinHandle<()>>::new()));
+    // TODO maybe don't need to do Arc Mutex
+    let subscriptions = Arc::new(Mutex::new(HashMap::<u64, tokio::task::JoinHandle<Result<(), EthRpcError>>>::new()));
 
     while let Some(message) = recv_in_client.recv().await {
-        let id = message.id.clone();
-        let our_name = our_name.clone();
-        let send_to_loop = send_to_loop.clone();
+        let our = our.clone();
         let ws_rpc = ws_rpc.clone();
+        let send_to_loop = send_to_loop.clone();
         let print_tx = print_tx.clone();
-        tokio::spawn(async move {
-            if let Err(e) = handle_message(
-                our_name.clone(),
-                send_to_loop.clone(),
-                message,
-                ws_rpc,
-                print_tx,
-            ).await {
-                send_to_loop.send(
-                    make_error_message(
-                        our_name,
-                        id,
-                        "eth_rpc".to_string(),
-                        e,
-                    )
-                ).await.unwrap();
-            }
-        });
-    }
-}
 
-async fn handle_message(
-    our: String,
-    send_to_loop: MessageSender,
-    wm: WrappedMessage,
-    ws_rpc: Provider<Ws>,
-    print_tx: PrintSender,
-) -> Result<(), EthRpcError>  {
-    print_tx.send(Printout {
-        verbosity: 0,
-        content: "eth_rpc: got message".to_string(),
-    }).await;
-    let WrappedMessage { ref id, target: _, ref rsvp, message: Ok(Message { ref source, ref content }), }
-        = wm else {
-            return Err(EthRpcError::Error { error: format!("eth_rpc: couldn't parse message: {:?}", wm) });
+        print_tx.send(Printout {
+            verbosity: 0,
+            content: "eth_rpc: got message".to_string(),
+        }).await;
+        let WrappedMessage { ref id, target: _, ref rsvp, message: Ok(Message { ref source, ref content }), }
+            = message else {
+                panic!("foo"); // return Err(EthRpcError::Error { error: format!("eth_rpc: couldn't parse message: {:?}", wm) });
+            };
+    
+        let target = match content.message_type {
+            MessageType::Response => {
+                panic!("foo"); // return Err(EthRpcError::Error { error: format!("eth_rpc: got response, expected request: {:?}", wm) });
+            },
+            MessageType::Request(is_expecting_response) => {
+                if is_expecting_response {
+                    ProcessNode {
+                        node: our.clone(),
+                        process: source.process.clone(),
+                    }
+                } else {
+                    let Some(rsvp) = rsvp else {
+                        panic!("foo"); // return Err(EthRpcError::Error { error: format!("eth_rpc: got request with no RSVP: {:?}", wm)});
+                    };
+                    rsvp.clone()
+                }
+            }
+        };
+    
+        // let call_data = content.payload.bytes.content.clone().unwrap_or(vec![]);
+    
+        let Some(json) = content.payload.json.clone() else {
+            panic!("foo"); // return Err(EthRpcError::Error { error: format!("eth_rpc: request must have JSON payload, got: {:?}", wm) });
+        };
+    
+        let Ok(action) = serde_json::from_value::<EthRpcAction>(json.clone()) else {
+            panic!("foo"); // return Err(EthRpcError::Error { error: format!("eth_rpc: couldn't parse json: {:?}", json) });
         };
 
-    let target = match content.message_type {
-        MessageType::Response => {
-            return Err(EthRpcError::Error { error: format!("eth_rpc: got response, expected request: {:?}", wm) });
-        },
-        MessageType::Request(is_expecting_response) => {
-            if is_expecting_response {
-                ProcessNode {
-                    node: our.clone(),
-                    process: source.process.clone(),
-                }
-            } else {
-                let Some(rsvp) = rsvp else {
-                    return Err(EthRpcError::Error { error: format!("eth_rpc: got request with no RSVP: {:?}", wm)});
-                };
-                rsvp.clone()
-            }
-        }
-    };
-
-    let call_data = content.payload.bytes.content.clone().unwrap_or(vec![]);
-
-    let Some(json) = content.payload.json.clone() else {
-        return Err(EthRpcError::Error { error: format!("eth_rpc: request must have JSON payload, got: {:?}", wm) });
-    };
-
-    let Ok(action) = serde_json::from_value::<EthRpcAction>(json.clone()) else {
-        return Err(EthRpcError::Error { error: format!("eth_rpc: couldn't parse json: {:?}", json) });
-    };
-
-    match action {
-        EthRpcAction::SubscribeBlocks => {
-            // TODO must make this a tokio thread so that we can cancel it
-            print_tx.send(Printout {
-                verbosity: 0,
-                content: "eth_rpc: subscribing to blocks".to_string(),
-            }).await;
-
-            // have to send this empty response to make process happy
-            send_to_loop.send(
-                WrappedMessage {
-                    id: id.clone(),
-                    target: target.clone(),
-                    rsvp: None,
-                    message: Ok(Message {
-                        source: ProcessNode {
-                            node: our.clone(),
-                            process: "eth_rpc".to_string(),
-                        },
-                        content: MessageContent {
-                            message_type: MessageType::Response,
-                            payload: Payload {
-                                json: None,
-                                bytes: PayloadBytes{
-                                    circumvent: Circumvent::False,
-                                    content: Some(vec![]),
-                                },
-                            },
-                        },
-                    }),
-                }
-            ).await.unwrap();
-
-            let mut stream = ws_rpc.subscribe_blocks().await.unwrap();
-
-            // send to target
-            // until they cancel
-
-            while let Some(block) = stream.next().await {
-                send_to_loop.send(
-                    WrappedMessage {
-                        id: rand::random(),
-                        target: target.clone(),
-                        rsvp: None,
-                        message: Ok(Message {
-                            source: ProcessNode {
-                                node: our.clone(),
-                                process: "eth_rpc".to_string(),
-                            },
-                            content: MessageContent {
-                                message_type: MessageType::Request(false),
-                                payload: Payload {
-                                    json: Some(json!({
-                                        "BlockSubscription": serde_json::to_value(block).unwrap()
-                                    })),
-                                    bytes: PayloadBytes{
-                                        circumvent: Circumvent::False,
-                                        content: None
-                                    },
-                                },
-                            },
-                        }),
-                    }
-                ).await.unwrap();
-            }
-        }
-        EthRpcAction::SubscribeEvents(sub) => {
-            print_tx.send(Printout {
-                verbosity: 0,
-                content: format!("eth_rpc: subscribing to events: {:?}", sub),
-            }).await;
-
-            // have to send this empty response to make process happy
-            send_to_loop.send(
-                WrappedMessage {
-                    id: id.clone(),
-                    target: target.clone(),
-                    rsvp: None,
-                    message: Ok(Message {
-                        source: ProcessNode {
-                            node: our.clone(),
-                            process: "eth_rpc".to_string(),
-                        },
-                        content: MessageContent {
-                            message_type: MessageType::Response,
-                            payload: Payload {
-                                json: None,
-                                bytes: PayloadBytes{
-                                    circumvent: Circumvent::False,
-                                    content: Some(vec![]),
-                                },
-                            },
-                        },
-                    }),
-                }
-            ).await.unwrap();
-
-            let mut filter = Filter::new();
-            if let Some(addresses) = sub.addresses {
-                filter = filter.address(ValueOrArray::Array(
-                    addresses
-                        .into_iter()
-                        .map(|s| s.parse().unwrap())
-                        .collect()
-                ));
-            }
-
-            // TODO is there a cleaner way to do all of this?
-            if let Some(event) = sub.event {
-                filter = filter.event(&event);
-            }
-            if let Some(topic1) = sub.topic1 {
-                filter = filter.topic1(topic1);
-            }
-            if let Some(topic2) = sub.topic2 {
-                filter = filter.topic2(topic2);
-            }
-            if let Some(topic3) = sub.topic3 {
-                filter = filter.topic3(topic3);
-            }
-
-            let Ok(mut stream) = ws_rpc.subscribe_logs(&filter).await else {
-                return Err(EthRpcError::Error { error: format!("eth_rpc: couldn't create event subscription") });
-            };
-
-            while let Some(event) = stream.next().await {
+        match action {
+            EthRpcAction::SubscribeEvents(sub) => {
                 print_tx.send(Printout {
                     verbosity: 0,
-                    content: format!("eth_rpc: got event: {:?}", event),
+                    content: format!("eth_rpc: subscribing to events: {:?}", sub),
                 }).await;
+
+                let id: u64 = rand::random();
                 send_to_loop.send(
                     WrappedMessage {
-                        id: rand::random(),
+                        id: id.clone(),
                         target: target.clone(),
                         rsvp: None,
                         message: Ok(Message {
@@ -269,39 +120,109 @@ async fn handle_message(
                                 process: "eth_rpc".to_string(),
                             },
                             content: MessageContent {
-                                message_type: MessageType::Request(false),
+                                message_type: MessageType::Response,
                                 payload: Payload {
-                                    json: Some(json!({
-                                        "EventSubscription": serde_json::to_value(event).unwrap()
-                                    })),
+                                    json: Some(json!(id)),
                                     bytes: PayloadBytes{
                                         circumvent: Circumvent::False,
-                                        content: None
+                                        content: None,
                                     },
                                 },
                             },
                         }),
                     }
                 ).await.unwrap();
+
+                let mut filter = Filter::new();
+                if let Some(addresses) = sub.addresses {
+                    filter = filter.address(ValueOrArray::Array(
+                        addresses
+                            .into_iter()
+                            .map(|s| s.parse().unwrap())
+                            .collect()
+                    ));
+                }
+
+                // TODO is there a cleaner way to do all of this?
+                if let Some(event) = sub.event {
+                    filter = filter.event(&event);
+                }
+                if let Some(topic1) = sub.topic1 {
+                    filter = filter.topic1(topic1);
+                }
+                if let Some(topic2) = sub.topic2 {
+                    filter = filter.topic2(topic2);
+                }
+                if let Some(topic3) = sub.topic3 {
+                    filter = filter.topic3(topic3);
+                }
+
+                let ws_rpc = ws_rpc.clone();
+
+                let handle = tokio::task::spawn(async move {
+                    let Ok(mut stream) = ws_rpc.subscribe_logs(&filter).await else {
+                        return Err(EthRpcError::Error { error: format!("eth_rpc: couldn't create event subscription") });
+                    };
+        
+                    while let Some(event) = stream.next().await {
+                        print_tx.send(Printout {
+                            verbosity: 0,
+                            content: format!("eth_rpc: got event: {:?}", event),
+                        }).await;
+                        send_to_loop.send(
+                            WrappedMessage {
+                                id: rand::random(),
+                                target: target.clone(),
+                                rsvp: None,
+                                message: Ok(Message {
+                                    source: ProcessNode {
+                                        node: our.clone(),
+                                        process: "eth_rpc".to_string(),
+                                    },
+                                    content: MessageContent {
+                                        message_type: MessageType::Request(false),
+                                        payload: Payload {
+                                            json: Some(json!({
+                                                "EventSubscription": serde_json::to_value(event).unwrap()
+                                            })),
+                                            bytes: PayloadBytes{
+                                                circumvent: Circumvent::False,
+                                                content: None
+                                            },
+                                        },
+                                    },
+                                }),
+                            }
+                        ).await.unwrap();
+                    }
+                    Ok(())
+                });
+                subscriptions.lock().await.insert(id, handle);
+            }
+            EthRpcAction::Unsubscribe(sub_id) => {
+                print_tx.send(Printout {
+                    verbosity: 0,
+                    content: format!("eth_rpc: unsubscribing from {}", sub_id),
+                }).await;
+
+                if let Some(handle) = subscriptions.lock().await.remove(&sub_id) {
+                    handle.abort();
+                } else {
+                    print_tx.send(Printout {
+                        verbosity: 0,
+                        content: format!("eth_rpc: no task found with id {}", sub_id),
+                    }).await;
+                }
             }
         }
-        EthRpcAction::Unsubscribe(sub_id) => {
-            // TODO
-            print_tx.send(Printout {
-                verbosity: 0,
-                content: format!("eth_rpc: unsubscribing: {:?}", sub_id),
-            }).await;
-        }
     }
-
-    Ok(())
 }
 
 //
 //  helpers
 //
 fn make_error_message(
-    our_name: String,
+    our: String,
     id: u64,
     source_process: String,
     error: EthRpcError,
@@ -309,13 +230,13 @@ fn make_error_message(
     WrappedMessage {
         id,
         target: ProcessNode {
-            node: our_name.clone(),
+            node: our.clone(),
             process: source_process,
         },
         rsvp: None,
         message: Err(UqbarError {
             source: ProcessNode {
-                node: our_name,
+                node: our,
                 process: "filesystem".into(),
             },
             timestamp: get_current_unix_time().unwrap(),  //  TODO: handle error?

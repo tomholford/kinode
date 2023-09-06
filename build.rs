b9781b8bb7039667fa7d4e23f5571e2c065a141c
hosted-fornet <hosted-fornet@protonmail.com>
2023-09-06 15:40:44 -0700

    fix building: all apps are wasi

--- build.rs
+++ build.rs
@@ -17,54 +17,11 @@ fn main() {
     }
 
     let pwd = std::env::current_dir().unwrap();
-    let wit_file = fs::read_to_string("wit/uqbar.wit").unwrap();
-
-    // Build wasm32-unknown-unknown apps.
-
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    const APPS: [&str; 2] = [
-        // "apps_home",
-        // // "file_transfer",
-        // // "file_transfer_one_off",
-        // // "hi_lus_lus",
-        // "http_bindings",
-        // "http_proxy",
-        "terminal",
-        // // "sequencer",
-        // "explorer",
-        "sequentialize",
-        // "persist",
-        // "file_transfer/ft_client",
-        // "file_transfer/ft_server",
-        // "file_transfer/ft_client_worker",
-        // "file_transfer/ft_server_worker",
-    ];
-    for name in APPS {
-        // only execute if one of the modules has source code changes
-        println!("cargo:rerun-if-changed=modules/{}/src", name);
-    }
-    for name in APPS {
-        // copy in the wit file
-        fs::write(
-            format!("{}/modules/{}/wit", pwd.display(), name),
-            wit_file.clone(),
-        )
-            .unwrap();
-        // build the component
-        run_command(Command::new("cargo").args(&[
-            "component",
-            "build",
-            "--release",
-            &format!("--manifest-path={}/modules/{}/Cargo.toml", pwd.display(), name),
-            "--target",
-            "wasm32-unknown-unknown",
-        ]))
-            .unwrap();
-    }
-
 
     // Build wasm32-wasi apps.
-    const WASI_APPS: [&str; 1] = [
+    const WASI_APPS: [&str; 3] = [
+        "terminal",
+        "sequentialize",
         "key_value",
     ];
     for name in WASI_APPS {
@@ -72,17 +29,15 @@ fn main() {
         println!("cargo:rerun-if-changed=modules/{}/src", name);
     }
     for name in WASI_APPS {
-        // copy in the wit file
-        fs::write(
-            format!("{}/modules/{}/wit", pwd.display(), name),
-            wit_file.clone(),
-        )
+        // copy in the wit files
+        run_command(Command::new("cp").args(&[
+            "-r",
+            "wit",
+            &format!("{}/modules/{}", pwd.display(), name),
+        ]))
             .unwrap();
 
-        run_command(Command::new("mkdir").args(&[
-            "-p",
-            &format!("{}/modules/{}/target/wasm32-unknown-unknown/release", pwd.display(), name),
-        ]))
+        fs::create_dir_all(&format!("{}/modules/{}/target/wasm32-unknown-unknown/release", pwd.display(), name))
             .unwrap();
 
         // build the module
@@ -105,7 +60,6 @@ fn main() {
             &format!("{}/modules/{}/target/wasm32-wasi/release/{}_adapted.wasm", pwd.display(), name, name),
             "--adapt",
             &format!("{}/wasi_snapshot_preview1.wasm", pwd.display()),
-            // "wasi_snapshot_preview1.wasm",
         ]))
             .unwrap();
 

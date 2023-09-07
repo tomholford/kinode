9a6d9e68d6618bf19db813696fe1aa5ec7339e86
dachus <dachus@dachus>
2023-09-07 12:03:15 -0600

    message to net working

--- modules/pqi_indexer/src/lib.rs
+++ modules/pqi_indexer/src/lib.rs
@@ -91,10 +91,41 @@ impl UqProcess for Component {
             match msg {
                 // anticipating more message types later
                 AllActions::EventSubscription(e) => {
+                    let pqi_id = hex_to_u64(&e.topics[1].to_string()).unwrap(); // TODO u64
+
                     let decoded = CreateEntry::decode_data(&decode_hex(&e.data).unwrap(), true).unwrap();
+                    let nft_contract = decoded.0;
+                    let nft_id       = decoded.1.to_string(); // TODO should probably stay a hex string...
+                    let public_key   = hex::encode(decoded.2);
+                    let (ip, port)   = split_ip_and_port(decoded.3);
+                    let routers      = decoded.4;
                     
+                    let json_payload = json!({
+                        "pqi_id": pqi_id,
+                        "nft_contract": nft_contract.to_string(),
+                        "nft_id": nft_id,
+                        "public_key": public_key,
+                        "ip": ip,
+                        "port": port,
+                        "routers": routers,
+                    }).to_string();
+
+                    bindings::print_to_terminal(0, format!("pqi_indexer: JSON {:?}", json_payload).as_str());
                     
-                    bindings::print_to_terminal(0, format!("pqi_indexer: DATA {:?}", f).as_str());
+                    send_request(
+                        &Address{
+                            node: our.node.clone(),
+                            process: ProcessId::Name("net".to_string()),
+                        },
+                        &Request{
+                            inherit: false,
+                            expects_response: false,
+                            metadata: None,
+                            ipc: Some(json_payload),
+                        },
+                        None,
+                        None, 
+                    );
                 }
             }
         }
@@ -102,6 +133,7 @@ impl UqProcess for Component {
 }
 
 // helpers
+// TODO these probably exist somewhere in alloy...not sure where though.
 fn decode_hex(s: &str) -> Result<Vec<u8>, hex::FromHexError> {
     // If the string starts with "0x", skip the prefix
     let hex_part = if s.starts_with("0x") {
@@ -110,4 +142,19 @@ fn decode_hex(s: &str) -> Result<Vec<u8>, hex::FromHexError> {
         s
     };
     hex::decode(hex_part)
+}
+
+fn hex_to_u64(hex: &str) -> Result<u64, std::num::ParseIntError> {
+    let without_prefix = if hex.starts_with("0x") {
+        &hex[2..]
+    } else {
+        hex
+    };
+    u64::from_str_radix(without_prefix, 16)
+}
+
+fn split_ip_and_port(combined: u64) -> (u32, u16) {
+    let port = (combined & 0xFFFF) as u16;              // Extract the last 16 bits
+    let ip = (combined >> 16) as u32;                   // Right shift to get the first 32 bits
+    (ip, port)
 }
